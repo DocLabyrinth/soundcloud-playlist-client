d63725c35b08a6b998efd4d43c1e829f995c57c7
DocLabyrinth <DocLabyrinth@github.com>
2012-05-09 22:43:12 +0100

    added delete button with confirmation to items on the playlist menu

--- src/view/PlaylistView.js
+++ src/view/PlaylistView.js
@@ -7,10 +7,14 @@ define(
 	function(Mustache, Backbone, PlaylistHTML) {
 		var PlaylistView = Backbone.View.extend({
 			events: {
-				'click a': 'onLinkClicked'
+				'click .select-link': 'onSelectLinkClicked',
+				'click .delete-link': 'onDeleteLinkClicked',
+				'click .alert button.close': 'onCancelLinkClicked',
+				'click .soft-del-link': 'onSoftDelLinkClicked'
 			},
 			tagName: 'li',
 			listElem: null,
+			confDiv: null,
 
 			initialize: function() {
 				this.model.bind('destroy', this.remove, this);
@@ -18,13 +22,32 @@ define(
 			render: function() {
 				var rendered = Mustache.render( PlaylistHTML, this.model.toJSON() );
 				this.$el.html(rendered);
+
+				this.confDiv = this.$el.find('.alert-error');
+				// start with the delete confirmation div hidden
+				this.confDiv.hide();
+
 				return this;
 			},
-			onLinkClicked: function(ev) {
+			onSelectLinkClicked: function(ev) {
 				ev.preventDefault();
 
 				// the event will be rippled up to the collection which contains this model
 				this.model.trigger('selected', this.model);
+			},
+			// the real delete link inside the confirmation alert
+			onDeleteLinkClicked: function(ev) {
+				ev.preventDefault();
+
+				this.remove();
+				this.model.destroy();
+			},
+			// the X in the confirmation alert
+			onCancelLinkClicked: function(ev) {
+				this.confDiv.hide();
+			},
+			onSoftDelLinkClicked: function() {
+				this.confDiv.show();
 			}
 		});
 
